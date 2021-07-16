196160d1951a3a1925f73d0cfb3aa9a4d1080c10
KhaledAlabssi <khaled.absi5@gmail.com>
2021-07-16 15:50:32 +0200

    adding react routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@
 import './App.css';
 import 'rsuite/dist/styles/rsuite-default.css'
 import {Button} from 'rsuite'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Button color='red' appearance='ghost'>HI</Button>
-      
-    </div>
+    <Router>
+      <div className="App">
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <h1>Login</h1>
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+
 
 export default App;
